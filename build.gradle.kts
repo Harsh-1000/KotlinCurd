7eda6d55a7abe37d15f4a98b0536ac909932570d
Harsh-1000 <harshgoku001@gmail.com>
2025-05-07 11:03:28 +0530

    modified StudentRegistrationException

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.postgresql:postgresql:42.7.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
 }
 
 tasks.test {
--- src/main/kotlin/exception/CustomException.kt
+++ src/main/kotlin/exception/CustomException.kt
@@ -6,6 +6,6 @@ sealed class CustomException(message: String) : Exception(message) {
     class InValidDataException(message: String = "Valid data is not provided") : CustomException(message)
     class StudentDBException(message: String= "Error Student DB"): CustomException(message)
     class CourseDBException(message: String= "Error Course DB"): CustomException(message)
-    class StudentRegistrationException(message: String = "StudentRegistrationError"): CustomException(message)
+    class StudentRegistrationException(message: String = "Error in register student"): CustomException(message)
 }
 
